26ee273e0e35056b31caad59caf8fbf9a7efaaec
runapal5 <runapal5@gmail.com>
2017-09-28 00:57:37 +0530

    Update query.py

--- data/query.py
+++ data/query.py
@@ -1,3 +1,3 @@
 import predictionio
 engine_client = predictionio.EngineClient(url="http://localhost:8000")
-print engine_client.send_query({"projectId": "P1", "testDataPath": "/quickstartapp/testData.txt" })
+print engine_client.send_query({"projectId": "P1", "testDataPath": "/quickstartapp/trainResult.txt" })
